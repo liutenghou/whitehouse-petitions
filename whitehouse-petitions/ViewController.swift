9a7a08278f309e7ade01b77a7854d1db57505d65
Leo Liu <leo.liu@aequilibrium.ca>
2018-06-26 13:20:53 -0700

    parse title and body of petition into tableView

--- whitehouse-petitions/ViewController.swift
+++ whitehouse-petitions/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import os.log
 import SwiftyJSON
 
 class ViewController: UITableViewController {
@@ -20,16 +21,31 @@ class ViewController: UITableViewController {
         let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
 
         if let url = URL(string: urlString){
-            if let data = try? String(contentsOf: url){
+            if let data = try? String(contentsOf: url){ //try incase nothing returned
                 let json = JSON(parseJSON: data)
                 
                 if json["metadata"]["responseInfo"]["status"].intValue == 200 {
                     //we're good to go
-                    print(json["metadata"]["responseInfo"]["developerMessage"])
+                    print(json["metadata"]["responseInfo"]["developerMessage"].stringValue)
+                    
+                    parse(json: json)
                 }
             }
         }
         
+    }
+    
+    func parse(json: JSON){
+        for result in json["results"].arrayValue{
+            let title = result["title"].stringValue
+            let body = result["body"].stringValue
+            let signatureCount = result["signatureCount"].stringValue //get it as string to put in dict
+            let petition = ["title": title, "body": body, "signatureCount": signatureCount]
+            
+            petitions.append(petition)
+        }
+        
+        tableView.reloadData()
     }
     
     //MARK:Tableview methods
@@ -39,8 +55,9 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = "Title goes here"
-        cell.detailTextLabel?.text = "Subtitle goes here"
+        let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition["title"]
+        cell.detailTextLabel?.text = petition["body"]
         
         return cell
     }
