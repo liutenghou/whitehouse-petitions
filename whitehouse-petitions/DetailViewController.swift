bdda95d2bb10697ac426f759dfc2c20c34b87b5b
Leo Liu <leo.liu@aequilibrium.ca>
2018-06-26 15:37:57 -0700

    add detailview

---
+++ whitehouse-petitions/DetailViewController.swift
@@ -0,0 +1,44 @@
+//
+//  DetailViewController.swift
+//  whitehouse-petitions
+//
+//  Created by Leo Liu on 6/26/18.
+//  Copyright © 2018 hungryforcookies. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+
+    var webView: WKWebView!
+    var detailItem: [String: String]!
+    
+    override func loadView() {
+        webView = WKWebView()
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard detailItem != nil else {
+            return
+        }
+        
+        //display petition body as html
+        if let body = detailItem["body"] {
+            var html = "<html>"
+            html += "<head>"
+            html += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
+            html += "<style> body { font-size: 150%; } </style>"
+            html += "</head>"
+            html += "<body>"
+            html += body
+            html += "</body>"
+            html += "</html>"
+            webView.loadHTMLString(html, baseURL: nil)
+        }
+    }
+
+}
