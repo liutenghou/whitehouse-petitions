d05c90a126f692fa598e3fc58a154adfc847cefe
Leo Liu <leo.liu@aequilibrium.ca>
2018-06-25 20:32:44 -0700

    connect to api.whitehouse.gov

--- whitehouse-petitions/ViewController.swift
+++ whitehouse-petitions/ViewController.swift
@@ -7,17 +7,42 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class ViewController: UITableViewController {
+    
+    //array of dictionaries for petitions
+    var petitions = [[String: String]]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        if let url = URL(string: urlString){
+            if let data = try? String(contentsOf: url){
+                let json = JSON(parseJSON: data)
+                
+                if json["metadata"]["responseInfo"]["status"].intValue == 200 {
+                    //we're good to go
+                    print(json["metadata"]["responseInfo"]["developerMessage"])
+                }
+            }
+        }
+        
+    }
+    
+    //MARK:Tableview methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Title goes here"
+        cell.detailTextLabel?.text = "Subtitle goes here"
+        
+        return cell
     }
 
 
