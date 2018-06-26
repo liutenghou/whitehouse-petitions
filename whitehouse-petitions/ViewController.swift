0812da3cc1becfabfaa66cdc761010d8ef983224
Leo Liu <leo.liu@aequilibrium.ca>
2018-06-25 17:52:31 -0700

    first commit

---
+++ whitehouse-petitions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  whitehouse-petitions
+//
+//  Created by Leo Liu on 6/25/18.
+//  Copyright © 2018 hungryforcookies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
